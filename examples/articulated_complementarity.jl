3f3cb5750ed5df77109c4206ffba50f4ac47434b
Robin Deits <robin.deits@gmail.com>
2017-08-17 19:01:45 -0400

    first working version with RBD models

--- examples/articulated_complementarity.jl
+++ examples/articulated_complementarity.jl
@@ -4,44 +4,38 @@ using Polyhedra
 using StaticArrays
 using JuMP, ConditionalJuMP, Cbc
 using Base.Test
+using RigidBodyDynamics
+using RigidBodyDynamics: colwise
+using Rotations
+using ForwardDiff
 
-rot2(θ) = SMatrix{2, 2}(cos(θ), -sin(θ), sin(θ), cos(θ))
-
-struct Obstacle{N, T, H <: HRepresentation{N, T}}
+struct Obstacle{T, H <: HRepresentation{3, T}}
     interior::H
-    contact_face::HalfSpace{N, T}
+    contact_face::HalfSpace{3, T}
+    μ::T
 end
 
-struct Environment{N, T, H1 <: HRepresentation{N, T}, H2 <: HRepresentation{N, T}}
-    obstacles::Vector{Obstacle{N, T, H1}}
-    free_regions::Vector{H2}
+struct ContactEnvironment{P <: Point3D, T, H <: HRepresentation{3, T}}
+    points::Vector{P}
+    obstacles::Vector{Obstacle{T, H}}
+    free_regions::Vector{H}
 end
 
-function contact_basis(face::HalfSpace{2})
+struct Environment{B <: RigidBody, C <: ContactEnvironment}
+    contacts::Dict{B, C}
+end
+
+function contact_basis(face::HalfSpace{3}, μ)
     θ = atan(μ)
-    R = rot2(θ)
+    R = RotY(θ)
     hcat(R * face.a, R' * face.a)
 end
 
-
-contact_basis(obs::Obstacle) = contact_basis(obs.contact_face)
+contact_basis(obs::Obstacle) = contact_basis(obs.contact_face, obs.μ)
 
 function ConditionalJuMP.Conditional(op::typeof(in), x::AbstractVector, P::HRepresentation)
     ConditionalJuMP.Conditional(&, [@?(P.A[i, :]' * x <= P.b[i]) for i in 1:length(P)]...)
 end
-
-
-# A simple complementarity-based time-stepping rigid body simulation. All
-# notation is taken from Stewart & Trinkle "An Implicit Time-Stepping Scheme for
-# Rigid Body Dynamics with Coulomb Friction".
-
-const h = 0.05
-const μ = 0.5
-const n = [0, 1]
-const mass = 1.0
-const g = [0, -9.81]
-const min_leg_len = 0.5
-const max_leg_len = 1.5
 
 struct ContactResult{T, Tf}
     β::Vector{T}
@@ -58,6 +52,7 @@ function JuMP.setvalue(contact::ContactResult{<:JuMP.AbstractJuMPScalar}, seed::
     @assert getvalue(contact.contact_force) ≈ seed.contact_force
 end
 
+JuMP.getvalue(f::FreeVector3D) = FreeVector3D(f.frame, getvalue(f.v))
 
 struct JointLimitResult{T, Tf <: AbstractVector}
     λ::T
@@ -70,18 +65,23 @@ function JuMP.setvalue(r::JointLimitResult{<:JuMP.AbstractJuMPScalar}, seed::Joi
     @assert getvalue(r.generalized_force) ≈ seed.generalized_force
 end
 
-struct LCPUpdate{T, Tf}
-    q::Vector{T}
-    v::Vector{T}
-    contacts::Vector{ContactResult{T, Tf}}
-    joint_contacts::Vector{JointLimitResult{T, Tf}}
+function JuMP.getvalue(x::MechanismState{<:JuMP.AbstractJuMPScalar})
+    MechanismState(x.mechanism, getvalue(configuration(x)), getvalue(velocity(x)), getvalue(additional_state(x)))
 end
 
+struct LCPUpdate{T, M <: MechanismState{T}, Tf}
+    state::M
+    contacts::Vector{ContactResult{T, Tf}}
+    # joint_contacts::Vector{JointLimitResult{T, Tf}}
+end
+
+# JuMP.getvalue(up::LCPUpdate) =
+#     LCPUpdate(getvalue(up.state), getvalue.(up.contacts), getvalue.(up.joint_contacts))
 JuMP.getvalue(up::LCPUpdate) =
-    LCPUpdate(getvalue.((up.q, up.v))..., getvalue.(up.contacts), getvalue.(up.joint_contacts))
+    LCPUpdate(getvalue(up.state), getvalue.(up.contacts))
+
 function JuMP.setvalue(up::LCPUpdate{<:JuMP.AbstractJuMPScalar}, seed::LCPUpdate{<:Number})
-    setvalue(up.q, seed.q)
-    setvalue(up.v, seed.v)
+    setvalue(up.state, seed.state)
     setvalue.(up.contacts, seed.contacts)
     setvalue.(up.joint_contacts, seed.joint_contacts)
 end
@@ -94,7 +94,12 @@ function leg_velocity_in_world(v)
     v[1:2] + [0, -1] * v[3]
 end
 
-function contact_force(qnext, vnext, obstacle::Obstacle, model::Model)
+struct ContactPoint{Tb <: RigidBody, Tp <: Point3D}
+    body::Tb
+    point::Tp
+end
+
+function resolve_contact(xnext::MechanismState, contact_point::ContactPoint, obstacle::Obstacle, model::Model, x_dynamics::MechanismState{<:Number})
     n = obstacle.contact_face.a
     D = contact_basis(obstacle)
     k = size(D, 2)
@@ -103,12 +108,29 @@ function contact_force(qnext, vnext, obstacle::Obstacle, model::Model)
     λ = @variable(model,          lowerbound=0,   basename="λ",     upperbound=100)
     c_n = @variable(model,        lowerbound=0,   basename="c_n",   upperbound=100)
 
-    separation = n' * leg_position_in_world(qnext) - obstacle.contact_face.β
-    contact_velocity = leg_velocity_in_world(vnext)
+    function separation_in_world(x)
+        q = x[1:num_positions(xnext)]
+        v = x[(num_positions(xnext)+1):end]
+        x_diff = MechanismState(xnext.mechanism, q, v)
+        point_in_world = transform_to_root(x_diff, contact_point.point.frame) * contact_point.point
+        separation = n' * point_in_world.v - obstacle.contact_face.β
+    end
+
+    separation = separation_in_world(state_vector(x_dynamics)) + (state_vector(xnext) - state_vector(x_dynamics))' * ForwardDiff.gradient(separation_in_world, state_vector(x_dynamics))
+
+    function contact_velocity_in_world(x)
+        q = x[1:num_positions(xnext)]
+        v = x[(num_positions(xnext)+1):end]
+        x_diff = MechanismState(xnext.mechanism, q, v)
+        point_in_world = transform_to_root(x_diff, contact_point.point.frame) * contact_point.point
+        contact_velocity = point_velocity(twist_wrt_world(x_diff, contact_point.body), point_in_world).v
+    end
+
+    contact_velocity = contact_velocity_in_world(state_vector(x_dynamics)) + ForwardDiff.jacobian(contact_velocity_in_world, state_vector(x_dynamics)) * (state_vector(xnext) - state_vector(x_dynamics))
 
     @constraints model begin
         λ .+ D' * contact_velocity .>= 0 # (8)
-        μ * c_n .- sum(β) >= 0 # (9)
+        obstacle.μ * c_n .- sum(β) >= 0 # (9)
     end
 
     @disjunction(model, (separation == 0), (c_n == 0)) # (10)
@@ -116,9 +138,9 @@ function contact_force(qnext, vnext, obstacle::Obstacle, model::Model)
     for j in 1:k
         @disjunction(model, ((λ + Dtv[j]) == 0), β[j] == 0) # (11)
     end
-    @disjunction(model, (μ * c_n - sum(β) == 0), (λ == 0)) # (12)
+    @disjunction(model, (obstacle.μ * c_n - sum(β) == 0), (λ == 0)) # (12)
 
-    contact_force = c_n * n .+ D * β
+    contact_force = FreeVector3D(root_frame(xnext.mechanism), c_n * n .+ D * β)
     ContactResult(β, λ, c_n, contact_force)
 end
 
@@ -135,46 +157,82 @@ function join_limits(qnext, vnext, limits::SimpleHRepresentation, model::Model)
     [joint_limit(qnext, vnext, limits.A[i, :], limits.b[i], model) for i in 1:length(limits)]
 end
 
-function update(q, v, u, env::Environment, model::Model)
-    qnext = @variable(model, [1:length(q)], lowerbound=-10, basename="qnext", upperbound=10)
-    vnext = @variable(model, [1:length(v)], lowerbound=-10, basename="vnext", upperbound=10)
+function update(x::MechanismState{X, M}, u, env::Environment, Δt::Real, model::Model, x_dynamics=x) where {X, M}
+    mechanism = x.mechanism
+    world = root_body(mechanism)
+    qnext = @variable(model, [1:num_positions(x)], lowerbound=-10, basename="qnext", upperbound=10)
+    vnext = @variable(model, [1:num_velocities(x)], lowerbound=-10, basename="vnext", upperbound=10)
+    xnext = MechanismState(mechanism, qnext, vnext)
 
-    contacts = [contact_force(qnext, vnext, obs, model) for obs in env.obstacles]
-    external_force = sum([c.contact_force for c in contacts])
+    contact_results = []
+    externalwrenches_list = []
+    for (body, contact_env) in env.contacts
+        geo_jac = geometric_jacobian(x_dynamics, path(mechanism, body, world))
+        wrenches = []
 
-    join_limit_results = join_limits(qnext, vnext, SimpleHRepresentation([0. 0 1; 0 0 -1], [1.5, -0.5]), model)
+        for contact_point in contact_env.points
+            for obs in contact_env.obstacles
+                result = resolve_contact(xnext, ContactPoint(body, contact_point), obs, model, x_dynamics)
+                push!(contact_results, result)
+                push!(wrenches, Wrench(
+                    transform_to_root(x_dynamics, contact_point.frame) * contact_point, 
+                    result.contact_force))
+            end
 
-    internal_force = u + sum([r.generalized_force[3] for r in join_limit_results])
+            function _point_in_world(x)
+                q = x[1:num_positions(xnext)]
+                v = x[(num_positions(xnext)+1):end]
+                x_diff = MechanismState(xnext.mechanism, q, v)
+                point_in_world = transform_to_root(x_diff, contact_point.frame) * contact_point
+                point_in_world.v
+            end
 
-    @constraints model begin
-        mass * (vnext[1:2] - v[1:2]) .== h * mass * g .- [0, -1] * internal_force  # (5)
-        mass * (vnext[3] - v[3]) == [0, -1]' * external_force + internal_force
-        qnext - q .== h .* vnext # (6)
+            point_in_world = _point_in_world(state_vector(x_dynamics)) + ForwardDiff.jacobian(_point_in_world, state_vector(x_dynamics)) * (state_vector(xnext) - state_vector(x_dynamics))
+
+            ConditionalJuMP.disjunction!(model,
+                [@?(point_in_world ∈ P) for P in contact_env.free_regions]) # (7)
+        end
+        push!(externalwrenches_list, (body => sum(wrenches)))
+    end
+    externalwrenches = Dict(externalwrenches_list)
+    contact_results_type = typeof(contact_results[1])
+    contact_results_typed = convert(Vector{contact_results_type}, contact_results)
+
+
+    H = mass_matrix(x_dynamics)
+    bias = dynamics_bias(x_dynamics, externalwrenches)
+
+    @constraint(model, H * (vnext - velocity(x)) .== Δt * (u .- bias)) # (5)
+
+    function _config_derivative(v)
+        q = oftype(v, configuration(x_dynamics))
+        x_diff = MechanismState(mechanism, q, v)
+        configuration_derivative(x_diff)
     end
 
-    # @constraints model begin
-    #     mass * (vnext - v) .== h * total_force # (5)
-    #     qnext - q .== h .* vnext # (6)
-    # end
+    config_derivative = ForwardDiff.jacobian(_config_derivative, velocity(x_dynamics)) * velocity(xnext)
+    @constraint(model, qnext .- configuration(x) .== Δt .* config_derivative)
 
-    ConditionalJuMP.disjunction!(
-        model,
-        [@?(leg_position_in_world(qnext) ∈ P) for P in env.free_regions]) # (7)
+    LCPUpdate(xnext, contact_results_typed)
 
-    LCPUpdate(qnext, vnext, contacts, join_limit_results)
+    # joint_limit_results = join_limits(qnext, vnext, SimpleHRepresentation([0. 0 1; 0 0 -1], [1.5, -0.5]), model)
+
+    # internal_force = u + sum([r.generalized_force[3] for r in joint_limit_results])
+
+    # LCPUpdate(qnext, vnext, contact_results, joint_limit_results)
 end
 
-function simulate(q0, v0, controller, env::Environment, N)
-    q, v = q0, v0
-    results = LCPUpdate{Float64}[]
+function simulate(x0::MechanismState, controller, env::Environment, Δt::Real, N::Integer)
+    results = []  # todo: type this
+    x = x0
     for i in 1:N
         m = Model(solver=CbcSolver())
-        u = controller(q, v)
-        up = update(q, v, u, env, m)
+        u = controller(x)
+        up = update(x, u, env, Δt, m)
+        # up = update(q, v, u, env, m)
         solve(m)
         push!(results, getvalue(up))
-        q = results[end].q
-        v = results[end].v
+        x = results[end].state
     end
     results
 end
@@ -209,50 +267,5 @@ function optimize(q0, v0, env::Environment, seed::Vector{<:LCPUpdate})
     solve(m)
     getvalue.(results)
 end
-
-
-# env = Environment(
-#     [
-#         Obstacle(
-#             SimpleHRepresentation{2, Float64}([0 1], [0]),
-#             HalfSpace{2, Float64}([0, 1], 0)
-#         ),
-#         Obstacle(
-#             SimpleHRepresentation{2, Float64}([-1 0], [-0.2]),
-#             HalfSpace{2, Float64}([-1, 0], -0.2)
-#         )
-#     ],
-#     [
-#         SimpleHRepresentation{2, Float64}(
-#             [0 -1;
-#              1 0],
-#             [0, 0.2])
-#     ]
-# )
-
-# q0 = [-0.5, 0.5]
-# v0 = [3, -1.5]
-# N = 20
-# results1 = simulate(q0, v0, env, N)
-# results2 = optimize(q0, v0, env, N)
-# @test all([r1.q ≈ r2.q for (r1, r2) in zip(results1, results2)])
-
-# results_seeded = optimize(q0, v0, env, results1)
-# @test all([r1.q ≈ r2.q for (r1, r2) in zip(results1, results_seeded)])
-
-# if Pkg.installed("DrakeVisualizer") !== nothing
-#     @eval using DrakeVisualizer;
-#     @eval using CoordinateTransformations
-#     DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window()
-
-#     vis = Visualizer()[:block]
-#     setgeometry!(vis, HyperRectangle(Vec(-0.1, -0.1, 0), Vec(0.2, 0.2, 0.2)))
-
-#     q = [r.q for r in results1]
-#     for qi in q
-#         settransform!(vis, Translation(qi[1], 0, qi[2]))
-#         sleep(h)
-#     end
-# end
 
 end
