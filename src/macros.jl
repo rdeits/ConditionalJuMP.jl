e482d070c5a3539bdc0ca1261444c31815d3c510
Robin Deits <robin.deits@gmail.com>
2017-09-27 18:18:36 -0400

    don't use expand() in macros, just handle two broadcast cases manually

--- src/macros.jl
+++ src/macros.jl
@@ -1,10 +1,23 @@
 _conditionalize(ex) = esc(ex)
 
+is_function_broadcast(ex::Expr) = ex.head == :(.) && ex.args[2].head == :tuple
+is_operator_broadcast(ex::Expr) = ex.head == :(call) && isa(ex.args[1], Symbol) && string(ex.args[1])[1] === '.'
+
+function _conditionalize_function_broadcast(ex::Expr)
+    Expr(:call, :_all_conditional, Expr(:call, GlobalRef(Base, :broadcast), :_conditional, esc(ex.args[1]), esc.(ex.args[2].args)...))
+end
+
+function _conditionalize_operator_broadcast(ex::Expr)
+    Expr(:call, :_all_conditional, Expr(:call, GlobalRef(Base, :broadcast), :_conditional, esc(Symbol(string(ex.args[1])[2:end])), esc.(ex.args[2:end])...))
+end
+
 function _conditionalize(ex::Expr)
-    if ex.head == :call 
-        if ex.args[1] == GlobalRef(Base, :broadcast)
-            Expr(:call, :_all_conditional, Expr(:call, GlobalRef(Base, :broadcast), :_conditional, esc.(ex.args[2:end])...))
-        elseif ex.args[1] == :(=>)
+    if is_function_broadcast(ex)
+        _conditionalize_function_broadcast(ex)
+    elseif is_operator_broadcast(ex)
+        _conditionalize_operator_broadcast(ex)
+    elseif ex.head == :call
+        if ex.args[1] == :(=>)
             Expr(:call, :(=>), _conditionalize.(ex.args[2:end])...)
         else
             Expr(:call, :_conditional, _conditionalize.(ex.args)...)
@@ -15,22 +28,22 @@ function _conditionalize(ex::Expr)
 end
 
 macro ?(ex)
-    _conditionalize(expand(ex))
+    _conditionalize(ex)
 end
 
 
 macro implies(m, args...)
-    Expr(:call, :implies!, esc(m), _conditionalize.(expand.(args))...)
+    Expr(:call, :implies!, esc(m), _conditionalize.(args)...)
 end
 
 macro disjunction(m, args...)
-    Expr(:call, :disjunction!, esc(m), Expr(:tuple, _conditionalize.(expand.(args))...))
+    Expr(:call, :disjunction!, esc(m), Expr(:tuple, _conditionalize.(args)...))
 end
 
 macro switch(args...)
-    Expr(:call, :switch, _conditionalize.(expand.(args))...)
+    Expr(:call, :switch, _conditionalize.(args)...)
 end
 
 macro ifelse(c, v1, v2)
-    Expr(:call, :ifelse, _conditionalize(expand(c)), v1, v2)
+    Expr(:call, :ifelse, _conditionalize(c), v1, v2)
 end
